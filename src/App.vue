705343a485a47db1235e78a0b48d45f76e0b0700
Johnk-Mj <johnkatembue4@gmail.com>
2023-08-30 13:08:11 +0200

    init repo

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { createHead } from 'unhead'
+import '@unocss/reset/tailwind-compat.css'
+
+createHead()
+</script>
+
+<template>
+  <Layout/>
+</template>
+
---
+++ uno.config.js
@@ -0,0 +1,34 @@
+// uno.config.ts
+import {
+  defineConfig, presetAttributify, presetIcons, presetUno, presetMini, presetWebFonts
+} from 'unocss'
+
+import transformerDirectives from '@unocss/transformer-directives'
+
+export default defineConfig({
+  transformers: [
+    transformerDirectives(),
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetMini({
+      dark: "class"
+    }),
+    presetWebFonts({
+      provider: "none",
+      fonts: {
+        dms: ["'DM Sans'", 'sans-serif'],
+        poppins: [],
+        display: ['Oswald', 'sans-serif']
+      },
+    }),
+    presetIcons(
+      {
+        collections: {
+          carbon: () => import('@iconify-json/carbon/icons.json').then(i => i.default),
+        }
+      }
+    ),
+  ],
+})
