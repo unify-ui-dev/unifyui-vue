bf23e5ad12eb2f94bc3ec8ba2fd1793ca8e56eea
Johnk-Mj <johnkatembue4@gmail.com>
2024-01-28 11:34:38 +0200

    ✨ [BREAKING] Warp speed ahead!  Integrate RadixVue & UnifyUI ⚡️

--- components/uno.config.ts
+++ components/uno.config.ts
@@ -1,5 +1,23 @@
-// uno.config.ts
-import { defineConfig } from 'unocss'
+import { defineConfig, presetUno, presetAttributify, presetMini, presetIcons } from 'unocss'
+
+import transformerDirectives from '@unocss/transformer-directives'
+
 export default defineConfig({
-  
+    transformers: [
+        transformerDirectives(),
+    ],
+    presets: [
+        presetUno(),
+        presetAttributify(),
+        presetMini({
+            dark: "class"
+        }),
+        presetIcons(
+            {
+                collections: {
+                    carbon: () => import('@iconify-json/carbon/icons.json').then(i => i.default),
+                }
+            }
+        ),
+    ]
 })
